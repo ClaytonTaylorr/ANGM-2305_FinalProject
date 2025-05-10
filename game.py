004f212ff253b3bf0e59e3b024844d4dde0f285c
Clayton Taylor <claytontaylorr993@gmail.com>
2025-05-10 14:41:29 -0500

    Added folder path, sprites, tileable background feature and calling for it in the main loop

--- game.py
+++ game.py
@@ -5,17 +5,38 @@ from os import listdir
 from os.path import isfile, join
 pygame.init()
 
+
 pygame.display.set_caption("ANGM 2305 Final - Sidescrolling Platformer")
 
-BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1280, 720
 FPS = 60
 PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+# calls for the background, first letting pygame know im using the assets folde and the background folde inside
+# using intigers to tile the images by multiplying its position, top left corner, to its height & width to move said tile
+def get_background(name):
+    image = pygame.image.load(join("asset", "Background", name))
+    _, _, width, height, = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    return tiles, image
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+# make sure to update the display so images dont stay rendered off screen, slowing the game down. Learned that in the matrix screensaver project
+    pygame.display.update()
+
 def main(window):
     clock = pygame.time.Clock()
+    background = get_background("Blue_BG.png")
 
     run = True
     while run:
@@ -25,6 +46,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+    draw(window, background, bg_image)
     pygame.quit()
     quit()
 
