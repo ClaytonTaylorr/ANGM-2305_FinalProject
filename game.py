f857e0198a0949146c80d4ae3e69e13fb3c22820
Clayton Taylor <claytontaylorr993@gmail.com>
2025-05-11 17:56:28 -0500

    Added more sprites. Added a block that loads sprites per individual frame, and flips them based on said direction.

--- game.py
+++ game.py
@@ -14,7 +14,35 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
-# 
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False)for sprite in sprites]
+
+# load all files(sprite sheets) in the directory for the block to use to display
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+        # Selecting only the individual frames out of the sprite sheets by multiplying by its width
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0,0), rect)
+            # Need to rid of the scale from earlier since scaling will be done here now
+            sprites.append(pygame.transform.scale2x(surface))
+        # Flips the sprite sheet for when changing character direction
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+
+    return all_sprites
+
 class Player(pygame.sprite.Sprite):
     COLOR = (255,0,0)
     GRAVITY = 1
